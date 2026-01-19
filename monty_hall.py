f1fd39d7c2be7f4e03ed58f24702811ff3b65c97
Garlic_Sticks <ap25012005@gmail.com>
2026-01-19 13:47:38 +0530

    monty hall

--- monty_hall.py
+++ monty_hall.py
@@ -2,7 +2,6 @@ import random
 
 doors = ['A','B','C']
 prize = random.choice(doors) #choosing correct door from options
-print(f'DEBUG: {prize}')
 while True:
     user = input(f'enter your choice {doors} \n').upper()
     if user in doors:
@@ -11,10 +10,8 @@ while True:
         print('enter a valid choice: A, B, or C')
 
 second_round = set(doors) - {user} - {prize} #options for second round
-print(f'DEBUG second round choices for random opening: {second_round}')
 monty_opens = random.choice(list(second_round))
 last_door = set(doors) - {user} - {monty_opens}
-print(f'DEBUG user choices to switch: {last_door}')
 print(f'opening door {monty_opens}, it is a dud!')
 new_choice = last_door.pop()
 switch = input(f'do you want to switch to door {new_choice}? (y/n) ')
