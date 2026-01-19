be4e8145b0019996279f1b2f55d5507b79b13888
Garlic_Sticks <ap25012005@gmail.com>
2026-01-19 12:45:57 +0530

    monty hall

--- monty_hall.py
+++ monty_hall.py
@@ -1,13 +1,8 @@
 import random
-from operator import truediv
 
 doors = ['A','B','C']
-#correct = [] #bucket to store winner
-#choice = []
 prize = random.choice(doors) #choosing correct door from options
-#correct.append(prize)
-print(prize)
-#user = input(f'enter your choice {doors} \n').upper()
+print(f'DEBUG: {prize}')
 while True:
     user = input(f'enter your choice {doors} \n').upper()
     if user in doors:
@@ -15,15 +10,16 @@ while True:
     else:
         print('enter a valid choice: A, B, or C')
 
-#choice.append(user) #adding user choice door to bucket
 second_round = set(doors) - {user} - {prize} #options for second round
-system_opens = random.choice(list(second_round))
-last_door = set(second_round) - set(system_opens)
-print(f'opening door {system_opens}, it is a dud!')
-switch = input(f'do you want to switch to door {last_door}? (y/n) ')
+print(f'DEBUG second round choices for random opening: {second_round}')
+monty_opens = random.choice(list(second_round))
+last_door = set(doors) - {user} - {monty_opens}
+print(f'DEBUG user choices to switch: {last_door}')
+print(f'opening door {monty_opens}, it is a dud!')
+new_choice = last_door.pop()
+switch = input(f'do you want to switch to door {new_choice}? (y/n) ')
 if switch == 'y':
-
-
+   user = new_choice
 if user == prize:
     print('you won!')
 else:
