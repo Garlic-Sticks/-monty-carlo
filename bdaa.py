c8720e82ad5c28a05946f60e7da3ee07cab3bf36
Garlic_Sticks <ap25012005@gmail.com>
2026-01-21 09:48:37 +0530

    die game rolls players and bars

--- bdaa.py
+++ bdaa.py
@@ -1,18 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-rolls_array = np.random.randint(1,7,size=(5,10))
-player_totals = np.sum(rolls_array, axis=1)
-print(f'scores: {player_totals}')
-#print(np.max(player_totals))
-players = np.array(['a', 'b', 'c', 'd', 'e'])
-plt.bar(players, player_totals, color='green')
-max_score = max(player_totals)
+def dice_game(players, rolls):
+    rolls_array = np.random.randint(1,7,size=(players,rolls))
+    player_totals = np.sum(rolls_array, axis=1)
+    return player_totals
+
+a = int(input('Enter a number of players: '))
+b = int(input('Enter rolls per player: '))
+game1 = dice_game(a,b)
+print(f'scores: {game1}')
+
+max_score = max(game1)
 print(f'max_score: {max_score}')
-winning_index= np.where(player_totals == max_score)[0]
-plt.title('dice game')
-plt.xlabel('players')
-plt.ylabel('total points:')
-print(f' winner is: {winning_index +1}')
-plt.bar(players[winning_index], player_totals[winning_index], color='blue')
+winning_index= np.where(game1 == max_score)[0]
+x_axis = np.arange(a) + 1
+plt.bar(x_axis,game1,color='blue')
+plt.bar(x_axis[winning_index],game1[winning_index],color='red')
+
+print(f' winner is: {winning_index + 1}')
 plt.show()
