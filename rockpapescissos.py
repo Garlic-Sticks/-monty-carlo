bf579dc793a46eb192daff28e48075cb8e4c4a5e
Garlic_Sticks <ap25012005@gmail.com>
2026-01-27 11:48:11 +0530

    rps v2 modulo

--- rockpapescissos.py
+++ rockpapescissos.py
@@ -1,24 +1,22 @@
 import random
-choices = ('r', 'p', 's')
-bot_choice = random.choice(choices)
-beats = {'r':'s',
-         'p':'r',
-         's':'p',}
-while True:
-    bot_choice = random.choice(choices)
-    #print(bot_choice)
-    user_choice = input('Enter a choice; rock(r), paper(p), scissors(s):\n ').lower()
-    if user_choice not in choices:
-        print('invalid choice, r p s only')
-        continue
-    #print(user_choice)
-    if user_choice == (bot_choice):
-        print('draw')
-    elif beats[user_choice] == (bot_choice):
-        print('win')
-    else:
-        print('lose')
-    retry = input('do you want to try again? (y/n): ').lower()
-    if retry != 'y':
-        print('see you again')
-        break
+
+user_choice = input("Enter your choice: ")
+choices = {'r':1,
+           'p':2,
+           's':0}
+bot_choice = random.choice(list(choices))
+print(f'bot choice {bot_choice}')
+bot_store = choices.get(bot_choice)
+user_store = choices.get(user_choice)
+# print(f'bot store {bot_store}, {type(bot_store)}')
+# print(f'user store {user_store}, {type(user_store)}')
+# print(f'user choice {user_choice}')
+decider = (user_store - bot_store) % 3
+if decider == 1:
+    print(f'you won, bot chose {bot_choice} and you chose {user_choice}')
+elif decider == 2:
+    print(f'you lost, bot chose {bot_choice} and you chose {user_choice}')
+else:
+    print('draw')
+
+
